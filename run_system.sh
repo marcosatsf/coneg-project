028f90c4908dac17f0a0a7767a68971f80ff4478
MarcosATSF <marcos_atsf@hotmail.com>
2021-09-21 23:44:20 -0300

    Stable version of system

---
+++ run_system.sh
@@ -0,0 +1,7 @@
+docker-compose down
+docker pull gcr.io/coneg-system/coneg-inspector-api
+docker pull gcr.io/coneg-system/coneg-panel-api
+docker pull gcr.io/coneg-system/coneg-postgres
+docker pull gcr.io/coneg-system/coneg-web-admin
+docker pull gcr.io/coneg-system/coneg-web-patra
+docker-compose up
